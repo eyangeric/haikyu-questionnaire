2eb50ca696240c9162ff65fb1b5e898be6591d50
eyangeric <eyangeric@gmail.com>
2024-03-06 22:26:02 -0600

    added some character score ranges

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-statements = {
+questions = {
     1: 'You have multiple interests that you are passionate about exploring.',
     2: 'Your natural instinct is to apply an analytical approach as opposed to going with gut feeling.',
     3: 'Regardless of the ups and downs, if you have an approach that has proven successful, you prefer to stick to your process to weather the storm.',
@@ -7,7 +7,7 @@ statements = {
     6: 'When you come across an alternative method, you feel a strong desire to add it to your repertoire.',
     7: 'You want to search out talented individuals to learn from and match up against.',
     8: 'When surrounded by more naturally, talented people, even though you might not be able to catch up to them, you feel a strong desire to maximize yourself.',
-    9: 'When there is an individual around you who you think is not putting in sufficient effort, you want to crush him/her.',
+    9: 'When there is an individual around you who you think is not putting in sufficient effort, you want to completely crush him/her.',
     10: 'When you have a goal, you are willing to do whatever amount of studying/life-hacking needed to reach it.'
 }
 
@@ -15,58 +15,58 @@ statements = {
 character_score_ranges = [
     {
         'Daichi Sawamura': {
-            1: (5, 7),
-            2: (8, 9),
-            3: (),
-            4: (),
-            5: (),
-            6: (),
-            7: (),
-            8: (),
-            9: (),
-            10: (),
+            1: (5, 6),
+            2: (7, 9),
+            3: (10, 10),
+            4: (9, 10),
+            5: (5, 7),
+            6: (5, 7),
+            7: (5, 7),
+            8: (6, 7),
+            9: (6, 6),
+            10: (7, 8)
         }
     },
     {
         'Koshi Sugawara': {
-            1: (6, 8),
+            1: (5, 6),
             2: (8, 9),
-            3: (),
-            4: (),
-            5: (),
-            6: (),
-            7: (),
-            8: (),
-            9: (),
-            10: (),
+            3: (10, 10),
+            4: (10, 10),
+            5: (5, 7),
+            6: (5, 7),
+            7: (5, 6),
+            8: (6, 8),
+            9: (5, 6),
+            10: (6, 8)
         }
     },
     {
         'Asahi Azumane': {
-            1: (5, 6),
-            2: (8, 9),
-            3: (),
-            4: (),
-            5: (),
-            6: (),
-            7: (),
-            8: (),
-            9: (),
-            10: (),
+            1: (6, 8),
+            2: (5, 7),
+            3: (3, 5),
+            4: (4, 7),
+            5: (6, 8),
+            6: (5, 7),
+            7: (5, 5),
+            8: (3, 7),
+            9: (2, 5),
+            10: (5, 7)
         }
     },
     {
         'Yu Nishinoya': {
             1: (8, 10),
-            2: (7, 9),
-            3: (),
-            4: (),
-            5: (),
-            6: (),
-            7: (),
-            8: (),
-            9: (),
-            10: (),
+            2: (1, 3),
+            3: (4, 6),
+            4: (1, 4),
+            5: (10, 10),
+            6: (10, 10),
+            7: (5, 7),
+            8: (9, 10),
+            9: (7, 10),
+            10: (8, 10)
         }
     },
     {
@@ -182,20 +182,6 @@ character_score_ranges = [
         }
     },
     {
-        'Kotaro Bokuto': {
-            1: (1, 1),
-            2: (),
-            3: (),
-            4: (),
-            5: (),
-            6: (),
-            7: (),
-            8: (),
-            9: (),
-            10: (),
-        }
-    },
-    {
         'Wakatoshi Ushijima': {
             1: (1, 1),
             2: (),
@@ -208,49 +194,6 @@ character_score_ranges = [
             9: (),
             10: (),
         }
-    },
-    {
-        'Shinsuke Kita': {
-            1: (10, 10),
-            2: (),
-            3: (),
-            4: (),
-            5: (),
-            6: (),
-            7: (),
-            8: (),
-            9: (),
-            10: (),
-        }
-    },
-    {
-        'Atsumu Miya': {
-            1: (1, 1),
-            2: (),
-            3: (),
-            4: (),
-            5: (),
-            6: (),
-            7: (),
-            8: (),
-            9: (),
-            10: (),
-        }
-    },
-    {
-        'Osamu Miya': {
-            1: (4, 7),
-            2: (),
-            3: (),
-            4: (),
-            5: (),
-            6: (),
-            7: (),
-            8: (),
-            9: (),
-            10: (),
-        }
     }
-
 ]
     
--- train_character_ml_model.py
+++ train_character_ml_model.py
@@ -3,7 +3,6 @@ import numpy as np
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.externals import joblib
 import joblib
 
 
